d79f20316a48853ff0f606f48eb55f2faf23f463
ankit-yadav90 <ankityadav73699@gmail.com>
2025-05-01 11:50:08 +0530

    practice

---
+++ first.js
@@ -0,0 +1,15 @@
+//qs 1
+let newbtn = document.createElement("button");
+newbtn.innerText = "click me"
+
+newbtn.style.backgroundColor = "red";
+newbtn.style.color = "white";
+
+document.querySelector("body").prepend(newbtn);
+
+// Qs 2
+let para = document.querySelector("p");
+
+
+
+
